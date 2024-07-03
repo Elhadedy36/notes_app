eeee305a86bc0c8af57c9d04e40ce22c887493ae
Elhadedy36 <elhadede36@gmail.com>
2024-07-03 03:26:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main ()
+{
+  runApp(Notes_app());
+}
+
+
+
+class Notes_app extends StatelessWidget {
+  const Notes_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
