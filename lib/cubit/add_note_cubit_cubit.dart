5d2a835cffa634f4395003a9c8fa211259a770c4
Elhadedy36 <elhadede36@gmail.com>
2024-07-18 23:06:25 +0300

    addnote provider for all screens in main

--- lib/cubit/add_note_cubit_cubit.dart
+++ lib/cubit/add_note_cubit_cubit.dart
@@ -6,8 +6,8 @@ import 'package:notes_app/constants.dart';
 
 part 'add_note_cubit_state.dart';
 
-class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
-  AddNoteCubitCubit() : super(AddNoteCubitInitial());
+class AddNoteCubit extends Cubit<AddNoteCubitState> {
+  AddNoteCubit() : super(AddNoteCubitInitial());
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Models/Note_Model.dart';
 import 'package:notes_app/Views/notes_view.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubit/add_note_cubit_cubit.dart';
 
 void main ()async
 {
@@ -19,12 +21,18 @@ class Notes_app extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,
-      fontFamily: 'assets/fonts/Poppins-Regular.ttf'
+    return MultiBlocProvider(
+      providers: 
+      [
+        BlocProvider(create: (context)=>AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark,
+        fontFamily: 'assets/fonts/Poppins-Regular.ttf'
+        ),
+        home:const Notesview(),
       ),
-      home:const Notesview(),
     );
   }
 }
