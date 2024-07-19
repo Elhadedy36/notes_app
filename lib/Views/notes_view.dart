5038b81ef923a606dcfa1048895204b5507d005b
Elhadedy36 <elhadede36@gmail.com>
2024-07-19 23:32:37 +0300

    create notes method & fetch method

--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/Widgets/Add_notes_buttonSheet.dart';
 import 'package:notes_app/Widgets/notes_view_body.dart';
+import 'package:notes_app/cubit/Notes%20cubit/notes_cubit.dart';
 
 class Notesview extends StatelessWidget {
   const Notesview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        child: Icon(Icons.add),
-        onPressed: ()
-        {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)) ,
-            context: context, builder: ((context) {
-            return AddNoteButtonSheet();
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          shape: CircleBorder(),
+          child: Icon(Icons.add),
+          onPressed: ()
+          {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)) ,
+              context: context, builder: ((context) {
+              return AddNoteButtonSheet();
+            }
+            ),
+            );
+      
           }
           ),
-          );
-
-        }
-        ),
-      body: Notesviewbody(),
+        body: Notesviewbody(),
+      ),
     );
   }
 }
--- lib/cubit/Notes cubit/notes_cubit.dart
+++ lib/cubit/Notes cubit/notes_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/Models/Note_Model.dart';
+import 'package:notes_app/constants.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+
+  fetchAllNotes()
+  async {
+
+    try {
+  Box<NoteModel> notesBox = Hive.box<NoteModel>(KNotesbox);
+ List<NoteModel> notes =  notesBox.values.toList();
+     emit(NotesSuccess(notes));
+}  catch (e) {
+  // TODO
+  emit(NotesFailure(e.toString()));
+}
+  }
 }
